9d0701c5ebf14e7bfe668ecd763dd8f77f379c5a
tsiorynobel <nobel.lebonheurdepatrice@gmail.com>
2025-09-19 10:39:55 +0200

    last version

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ function LogoBadge({ size = 120 }) {
       }}
     >
       <img
-        src="../public/img/logo.jpg"
+        src="img/logo.jpg"
         alt="Logo ZYPP"
         className="logo-img"
         style={{
